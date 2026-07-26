a157ecdc13c70860d31b9bc184ad66f013f75b11
itcgy <hello@itcgy.cn>
2026-07-26 13:53:12 +0800

    jwt工具类

--- src/main/java/com/starshop/common/utils/JwtUtils.java
+++ src/main/java/com/starshop/common/utils/JwtUtils.java
@@ -1,4 +1,48 @@
 package com.starshop.common.utils;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import java.util.Map;
+
 public class JwtUtils {
+
+    /**
+     * 生成jwt
+     * 使用Hs256算法, 私匙使用固定秘钥
+     *
+     * @param secretKey jwt 秘钥
+     * @param ttlMillis jwt过期时间(毫秒)
+     * @param claims    设置的信息
+     */
+    public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
+        //生成JWT时间
+        long l = System.currentTimeMillis() + ttlMillis;
+        //将long放进Data
+        Date date = new Date(l);
+        //构造结果返回
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setClaims(claims)
+                .setExpiration(date)
+                .compact();
+    }
+
+
+    /**
+     * Token解密
+     *
+     * @param secretKey jwt秘钥 此秘钥一定要保留好在服务端, 不能暴露出去, 否则sign就可以被伪造, 如果对接多个客户端建议改造成多个
+     * @param token     加密后的token
+     * @return
+     */
+    public static Claims parseJWT(String secretKey, String token) {
+         return Jwts.parser()
+                .setSigningKey(secretKey)
+                 // 设置需要解析的jwt
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }

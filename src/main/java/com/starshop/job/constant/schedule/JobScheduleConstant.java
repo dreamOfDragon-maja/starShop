1cd38b0a21dfb7064f047148033a0e2d40fbd0d3
itcgy <hello@itcgy.cn>
2026-09-17 10:12:55 +0800

    完成定时更新数据库最大商品ID

---
+++ src/main/java/com/starshop/job/constant/schedule/JobScheduleConstant.java
@@ -0,0 +1,7 @@
+package com.starshop.job.constant.schedule;
+
+public class JobScheduleConstant {
+
+    public static final String PREFIX_SCHEDULED_EXECUTOR_TASK = "定时任务执行：";
+
+}
---
+++ src/main/java/com/starshop/job/schedule/ExecutorUpdateMaxProductIdTask.java
@@ -0,0 +1,38 @@
+package com.starshop.job.schedule;
+
+import com.starshop.job.constant.common.JobCommonConstant;
+import com.starshop.job.constant.schedule.JobScheduleConstant;
+import com.starshop.job.sync.product.SyncProductFromEsToRedisService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ExecutorUpdateMaxProductIdTask {
+
+    private final ThreadPoolTaskExecutor executorSchedulerCommon;
+
+    private final SyncProductFromEsToRedisService syncProductFromEsToRedisService;
+
+    private static final String BUSINESS = "定时更新数据库最大商品id";
+
+
+    /**
+     * 定时更新数据库最大商品 ID
+     */
+    @Scheduled(cron = "0 0 0/1 * * ?")
+    public void scheduledUpdateMaxProductIdInDataCache() {
+        executorSchedulerCommon.execute(() -> {
+            log.info(JobScheduleConstant.PREFIX_SCHEDULED_EXECUTOR_TASK + BUSINESS
+                            + JobCommonConstant.THREAD_NAME +"{}"
+                            + JobCommonConstant.THREAD_ID +"{}"
+                    ,Thread.currentThread().getName()
+                    ,Thread.currentThread().getId());
+            syncProductFromEsToRedisService.syncMaxProductIdCache();
+        });
+    }
+}

cb9d63ec67bc1e17c85d3ba6e92cd36fa9bec4db
itcgy <hello@itcgy.cn>
2026-09-17 08:28:40 +0800

    完成初始化分类树服务器缓存

---
+++ src/main/java/com/starshop/job/init/CategoryTreeRedisCacheInitRunner.java
@@ -0,0 +1,26 @@
+package com.starshop.job.init;
+
+import com.starshop.service.CategoryService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CategoryTreeRedisCacheInitRunner  implements ApplicationRunner {
+
+    @Resource
+    private CategoryService categoryService;
+
+
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("初始化 分类树 服务器缓存...");
+        categoryService.updateCategoryTreeRedisCache();
+        log.info("初始化 分类树 服务器缓存成功...");
+    }
+
+
+}
